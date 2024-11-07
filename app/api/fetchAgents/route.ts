58663fc49904b8dec4eb2a745a6b9d3275482da2
fullupe <fullupe@gmail.com>
2024-11-07 11:46:23 +0100

    api updateda

--- app/api/fetchAgents/route.ts
+++ app/api/fetchAgents/route.ts
@@ -5,20 +5,39 @@
 const URL:string = process.env.NEXT_PUBLIC_BASE_URL_DATA as string
 
 
-export async function GET(req: Request, res: Response) {
-  try {
-    const response = await fetch(`${URL}getAgents`, { cache: 'no-store' });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+export async function GET(req: Request, res: Response){
+
+    try{
+        const response = await fetch(`${URL}getAgents`, { cache: 'no-store' });
+        const Agents = await response.json();
+        
+        return new Response(JSON.stringify(Agents))
+
+    }catch(error){
+       
+        return new Response(JSON.stringify({ error: 'Failed to fetch data' }), { status: 500 });
     }
 
-    const Agents = await response.json();
-    return new Response(JSON.stringify(Agents));
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return new Response(JSON.stringify({ error: 'Failed to fetch data' }), { status: 500 });
-  }
-}
 
-export const revalidate = 0;
+}
+export const revalidate =0;
+
+
+// export async function GET(req: Request, res: Response) {
+//   try {
+//     const response = await fetch(`${URL}getAgents`, { cache: 'no-store' });
+
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+
+//     const Agents = await response.json();
+//     return new Response(JSON.stringify(Agents));
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     return new Response(JSON.stringify({ error: 'Failed to fetch data' }), { status: 500 });
+//   }
+// }
+
+// export const revalidate = 0;
