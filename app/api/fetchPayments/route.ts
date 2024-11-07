6da040745c00bfe6b847b37254e21da70529f5ac
fullupe <fullupe@gmail.com>
2024-11-07 11:43:02 +0100

    api updated

--- app/api/fetchPayments/route.ts
+++ app/api/fetchPayments/route.ts
@@ -4,39 +4,39 @@ import { NextResponse } from "next/server"
 
 const URL:string = process.env.NEXT_PUBLIC_BASE_URL_DATA as string
 
-// export async function GET(req: Request, res: Response){
+export async function GET(req: Request, res: Response){
 
-//     try{
-//         const response = await fetch(`${URL}getPayments`,{ cache: 'no-store' })
-//         const Payments = await response.json()
-        
-//         return new Response(JSON.stringify(Payments))
-
-//     }catch(error){
-       
-//         return new Response(JSON.stringify({ error: 'Failed to fetch data' }), { status: 500 });
-//     }
-
-
-// }
-// export const revalidate =0;
-
-
-export async function GET(req: Request, res: Response) {
-    try {
+    try{
         const response = await fetch(`${URL}getPayments`,{ cache: 'no-store' })
-  
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-  
-      const Payments = await response.json();
-      return new Response(JSON.stringify(Payments));
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      return new Response(JSON.stringify({ error: 'Failed to fetch data' }), { status: 500 });
+        const Payments = await response.json()
+        
+        return new Response(JSON.stringify(Payments))
+
+    }catch(error){
+       
+        return new Response(JSON.stringify({ error: 'Failed to fetch data' }), { status: 500 });
     }
-  }
+
+
+}
+export const revalidate =0;
+
+
+// export async function GET(req: Request, res: Response) {
+//     try {
+//         const response = await fetch(`${URL}getPayments`,{ cache: 'no-store' })
   
-  export const revalidate = 0;
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//       }
+  
+//       const Payments = await response.json();
+//       return new Response(JSON.stringify(Payments));
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//       return new Response(JSON.stringify({ error: 'Failed to fetch data' }), { status: 500 });
+//     }
+//   }
+  
+//   export const revalidate = 0;
 
